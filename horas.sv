fa0c1204a952d607a263798ed6529baba2c4401f
Cleysso Schwanck <cleysso.s@edu.pucrs.br>
2025-08-30 18:18:15 -0300

    feat: incluido erro contador de horas ate 24

--- horas.sv
+++ horas.sv
@@ -2,7 +2,7 @@ module horas (
   input  logic clk_i,        // Clock principal
   input  logic rstn_i,       // Reset assíncrono ativo baixo
   input  logic inc_hora_i,   // Sinal de incremento vindo do módulo minutos
-  output logic [5:0] horas   // Contador de horas (0-23)
+  output logic [5:0] horas   // Contador de horas (0-24)
 );
 
   // Registrador interno
@@ -18,8 +18,8 @@ module horas (
     end else begin
       // Lógica de contagem de horas (acionada pelo pulso de incremento)
       if (inc_hora_i) begin
-        if (horas_reg == 6'd23) begin
-          horas_reg <= 6'b0; // Reinicia após 23 horas
+        if (horas_reg == 6'd24) begin
+          horas_reg <= 6'b0; // Reinicia após 24horas
         end else begin
           horas_reg <= horas_reg + 1'b1;
         end
